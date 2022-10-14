313c62d02e59d218add984bf1d0681c98ad66deb
Mustafa Zakria <mustafa.zakria21@gmail.com>
2022-10-14 19:56:44 +0200

    set up not first time navigate

--- app/src/main/java/com.udacity.shoestore/ui/MainActivity.kt
+++ app/src/main/java/com.udacity.shoestore/ui/MainActivity.kt
@@ -2,14 +2,38 @@ package com.udacity.shoestore.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.ActivityMainBinding
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        val navHostFragment = findViewById<View>(R.id.navHostFragment)
+
+        navHostFragment.findNavController()
+            .addOnDestinationChangedListener{ _, dest, _ ->
+                when(dest.id) {
+                    R.id.loginFragment, R.id.welcomeFragment, R.id.instructionsFragment ->
+                        binding.appBarLayout.visibility = View.GONE
+                    else -> binding.appBarLayout.visibility = View.VISIBLE
+                }
+            }
+
+
         Timber.plant(Timber.DebugTree())
     }
 }

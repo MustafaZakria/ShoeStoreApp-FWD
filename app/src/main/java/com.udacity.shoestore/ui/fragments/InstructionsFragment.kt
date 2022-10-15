be28a0a1a67456b725e93a25b4c9aab4a7120d6b
Mustafa Zakria <mustafa.zakria21@gmail.com>
2022-10-15 04:45:13 +0200

    implemented navigation code for onboarding screens

--- app/src/main/java/com.udacity.shoestore/ui/fragments/InstructionsFragment.kt
+++ app/src/main/java/com.udacity.shoestore/ui/fragments/InstructionsFragment.kt
@@ -5,16 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentInstructionsBinding
 
 class InstructionsFragment : Fragment() {
+
+    lateinit var binding: FragmentInstructionsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instructions, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_instructions, container, false)
+
+        binding.btnGetStarted.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_instructionsFragment_to_shoeListFragment)
+        )
+
+        return binding.root
     }
 
 

5e9101098d244aa76e80b57ed7bd418c859e6a52
Mustafa Zakria <mustafa.zakria21@gmail.com>
2022-10-24 15:25:10 +0200

    Added comments to layouts

--- app/src/main/java/com.udacity.shoestore/viewmodels/ShoeViewModel.kt
+++ app/src/main/java/com.udacity.shoestore/viewmodels/ShoeViewModel.kt
@@ -1,6 +1,5 @@
 package com.udacity.shoestore.viewmodels
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -8,7 +7,7 @@ import com.udacity.shoestore.models.Shoe
 
 class ShoeViewModel : ViewModel() {
 
-    val listOfShoes : LiveData<MutableList<Shoe>>
+    val listOfShoes: LiveData<MutableList<Shoe>>
         get() = _listOfShoes
 
     private val _listOfShoes = MutableLiveData<MutableList<Shoe>>()
